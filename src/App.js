442e72fb8f544d752a530f67dc451fa7c9a608ba
raflimuhammadd <raflimuhh09@gmail.com>
2022-08-18 12:37:51 +0700

    ADD: SCND

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import lake_bg from './assets/lake_bg.jpg';
+import coldBg from './assets/cold.jpg';
+import hot from './assets/hot.jpg';
+import hot2 from './assets/hot2.jpg';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{backgroundImage: `url(${lake_bg})`}}>
+      <div className="overlay">
+        <div className="container">
+          <div className="section section_input">
+            <input type="text" placeholder="Enter the City.." 
+            />
+            <button>°F</button>
+          </div>
+
+          <div className="section section_temperature">
+            <di className="icon">
+              <h3>Surabaya, ID</h3>
+              <img src="https://openweathermap.org/img/wn/02d@2x.png" 
+              alt="weatherIcon" />
+              <h3>Rainy</h3>
+            </di>
+            <div className="temperature">
+              <h1>49 °C</h1>
+            </div>
+          </div>
+
+          {/* bottom description */}
+        </div>
+      </div>
     </div>
   );
 }
